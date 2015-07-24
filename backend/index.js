28e6d2c2aea7d957c79551d4d3ecbc097ef8df06
roman <roman.ziva@gmail.com>
2015-07-24 16:56:02 +0300

    refs #8369 | Refactor twilio-sms-app to latest helper

--- backend/index.js
+++ backend/index.js
@@ -1,64 +1,88 @@
-var InnoHelper  = require('innometrics-helper'),
-    bodyParser  = require('body-parser'),
+var bodyParser  = require('body-parser'),
     express     = require('express'),
     util        = require('util'),
+    inno = require('innometrics-helper'),
     twilio      = require('twilio');
 
 var app = express();
-    app.use(bodyParser.json());
-    app.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        next();
-    });
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-
-var config = {
-    groupId: process.env.INNO_COMPANY_ID,
+var vars = {
     bucketName: process.env.INNO_BUCKET_ID,
-    appName: process.env.INNO_APP_ID,
     appKey: process.env.INNO_APP_KEY,
     apiUrl: process.env.INNO_API_HOST,
-    noCache: true
+    appName: process.env.INNO_APP_ID,
+    groupId: process.env.INNO_COMPANY_ID
 };
 
-var inno    = new InnoHelper(config);
+var innoHelper = new inno.InnoHelper(vars);
+
 var tclient = null;
 
 
-app.post('/', function(req) {
+app.post('/', function (req) {
 
-    getSettings(function(err, settings){
-        if (err) { throw err; }
+    getSettings(function (err, settings) {
+        var profile,
+            session,
+            event;
 
-        inno.getProfile(req.body, function(err, data){
-            if (err) { throw err; }
+        if (err) {
+            throw err;
+        }
 
-            if (settings.triggerEvent === data.event.definitionId) {
-                sendSms(settings, data, function(err, message){
-                    if (err) { throw err; }
-                    console.log("Message was send to " + message.to + ", status: " + message.status + ", text: " + message.body);
+        try {
+            profile = innoHelper.getProfileFromRequest(req.body);
+        } catch (e) {
+            throw e;
+        }
+
+        session = profile.getLastSession();
+        if (session) {
+            event = session.getLastEvent();
+
+            if (event && settings.triggerEvent === event.getDefinitionId()) {
+                sendSms(settings, {
+                    profileId: profile.getId(),
+                    section: session.getSession()
+                }, function (err, message) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(util.format(
+                        "Message was send to %s, status: %s, text: %s",
+                        message.to,
+                        message.status,
+                        message.body
+                    ));
                 });
             }
 
-        });
+        }
 
     });
 
 });
 
-var sendSms = function(settings, data, callback) {
+var sendSms = function (settings, data, callback) {
 
     // init twilio client
     tclient = new twilio(settings.twilioSid, settings.twilioToken);
 
     // get user data
     getUserDetails({
-        profileId:  data.profile.id,
-        section:    data.session.section,
+        profileId:  data.profileId,
+        section:    data.section,
         nameAttr:   settings.contactNameAttribute,
         phoneAttr:  settings.contactPhoneAttribute
-    }, function(err, user){
-        if (err) { throw err; }
+    }, function (err, user) {
+        if (err) {
+            throw err;
+        }
 
         var message = {
             body: util.format(settings.messageFormat, user.name),
@@ -66,7 +90,7 @@ var sendSms = function(settings, data, callback) {
             to: user.phone
         };
 
-        tclient.messages.create(message, function(err, message) {
+        tclient.messages.create(message, function (err, message) {
 
             if (err) {
                 err = new Error(err.message);
@@ -82,8 +106,8 @@ var sendSms = function(settings, data, callback) {
 
 };
 
-var getSettings = function(callback) {
-    inno.getAppSettings(function(err, settings){
+var getSettings = function (callback) {
+    innoHelper.getAppSettings(function (err, settings) {
 
         if (!err && !settings) {
             err = new Error('Failed to get settings from data handler');
@@ -102,26 +126,32 @@ var getSettings = function(callback) {
     });
 };
 
-var getUserDetails = function(opts, callback) {
-    inno.getProfileAttributes(opts, function(err, attributes){
-        var user = null;
+var getUserDetails = function (opts, callback) {
+    innoHelper.loadProfile(opts.profileId, function (error, profile) {
+        var user = null,
+            attributes;
 
-        if (!err) {
-            attributes = attributes.filter(function(attr){
-                return attr.section === opts.section && attr.data[opts.nameAttr] && attr.data[opts.phoneAttr];
+        if (!error) {
+            user = {};
+            attributes = profile.getAttributes(opts.nameAttr);
+            attributes.forEach(function (attribute) {
+                if (!user.name && attribute.getName() === opts.nameAttr && attribute.getValue()) {
+                    user.name = attribute.getValue();
+                    return;
+                }
+
+                if (!user.phone && attribute.getName() === opts.phoneAttr && attribute.getValue()) {
+                    user.phone = attribute.getValue();
+                }
             });
 
-            if (attributes.length) {
-                user = {
-                    name: attributes[0].data[opts.nameAttr],
-                    phone: attributes[0].data[opts.phoneAttr]
-                };
-            } else {
-                err = new Error('User not found');
+            if (!user.name || !user.phone) {
+                error = new Error('User not found');
+                user = null;
             }
         }
-        
-        callback(err, user);
+
+        callback(error, user);
     });
 };
 
